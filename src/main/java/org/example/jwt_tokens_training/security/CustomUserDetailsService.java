8f72c6f433b9ecb7967e2ca353c9202bd1dc8501
kasenka <kosenkoda37@mail.ru>
2025-07-21 22:21:59 +0300

    new

---
+++ src/main/java/org/example/jwt_tokens_training/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package org.example.jwt_tokens_training.security;
+
+
+import org.example.jwt_tokens_training.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> {
+                    return new UsernameNotFoundException("Юзер не найден");
+                });
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getLogin())
+                .password(user.getEncryptedPassword())
+//                .roles(user.getRoles())
+                .build();
+    }
+}
